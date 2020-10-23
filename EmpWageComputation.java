e2307f988af4d160c823a0b7408e7931481492c6
Sagar <sagarrashinkar2@gmail.com>
2020-10-23 23:36:26 +0530

    ADDED: daily employee wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,16 +5,21 @@ class EmpWageComputation {
 		System.out.println("Welcome To Employee Wage Computation");
 
 		int IS_FULL_TIME = 1;
+		int WAGE_PER_HOUR = 20;
+		int dailyWage = 0;
+		int workHour = 0;
 		double check = Math.floor(Math.random()*10)%2;
 
 		if(check == IS_FULL_TIME) {
 
-			System.out.println("Employee is present");
+			workHour = 8;
 		}
 		else {
 
-			System.out.println("Employee is not present");
+			workHour = 0;
 		}
+		dailyWage = workHour * WAGE_PER_HOUR;
+		System.out.println("Daily wage of employee is :"+ dailyWage);
 	}
 }
 
