88539b2e9e3269a5a30e41500d2820ec466e48a6
Sagar <sagarrashinkar2@gmail.com>
2020-10-23 23:50:13 +0530

    ADDED: part time and full time wage of employee

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,21 +5,30 @@ class EmpWageComputation {
 		System.out.println("Welcome To Employee Wage Computation");
 
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		int dailyWage = 0;
 		int workHour = 0;
-		double check = Math.floor(Math.random()*10)%2;
+
+		double check = Math.floor(Math.random()*10)%3;
 
 		if(check == IS_FULL_TIME) {
 
 			workHour = 8;
+			System.out.println("Employee is Full Time");
+		}
+		else if(check == IS_PART_TIME) {
+
+			workHour = 4;
+			System.out.println("Employee is Part Time");
 		}
 		else {
 
 			workHour = 0;
+			System.out.println("Employee is Not Present");
 		}
+
 		dailyWage = workHour * WAGE_PER_HOUR;
 		System.out.println("Daily wage of employee is :"+ dailyWage);
 	}
 }
-
