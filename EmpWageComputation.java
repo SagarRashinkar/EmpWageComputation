d5de04a1d2aa376e76387a7fbe5544fbba8ecd51
Sagar <sagarrashinkar2@gmail.com>
2020-10-24 00:57:08 +0530

    ADDED: wage for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,35 +1,40 @@
 class EmpWageComputation {
+	
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int WORK_DAYS_PER_MONTH = 20;
 
 	public static void main(String[] args) {
 
+	
 		System.out.println("Welcome To Employee Wage Computation");
 
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int WAGE_PER_HOUR = 20;
 		int dailyWage = 0;
 		int workHour = 0;
+		int totalWage = 0;
 
-		int check = (int)Math.floor(Math.random()*10)%3;
+		for(int i=0;i<WORK_DAYS_PER_MONTH;i++){
+			
+			int check = (int)Math.floor(Math.random()*10)%3;
 
-		switch(check) {
+			switch(check) {
+	
+				case 1:
+					workHour = 8;
+					break;
+				case 2:
+					workHour = 4;
+					break;
+				default:
+					workHour = 0;
+					break;
 
-			case 1:
-				workHour = 8;
-				System.out.println("Employee is Full Time");
-				break;
-			case 2:
-				workHour = 4;
-				System.out.println("Employee is Part Time");
-				break;
-			default:
-				workHour = 0;
-				System.out.println("Employee is Not Present");
-				break;
-
+			}
+			dailyWage = workHour * WAGE_PER_HOUR;
+			totalWage += dailyWage;
+			System.out.println("Daily wage of employee is :"+ dailyWage);
 		}
-
-		dailyWage = workHour * WAGE_PER_HOUR;
-		System.out.println("Daily wage of employee is :"+ dailyWage);
+		System.out.println("Monthly Wage is: "+totalWage);
 	}
 }
